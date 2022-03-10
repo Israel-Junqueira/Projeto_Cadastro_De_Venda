82b96e3d353a91024baee80f60ffab9eddea1975
Israel Ribeiro <israel.ribeiro313@gmail.com>
2022-03-10 14:12:36 -0300

    CRUD Scaffolding criado

---
+++ CadastroDeVendas/Data/CadastroDeVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CadastroDeVendas.Models;
+
+namespace CadastroDeVendas.Data
+{
+    public class CadastroDeVendasContext : DbContext
+    {
+        public CadastroDeVendasContext (DbContextOptions<CadastroDeVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CadastroDeVendas.Models.Department> Department { get; set; }
+    }
+}
--- CadastroDeVendas/Startup.cs
+++ CadastroDeVendas/Startup.cs
@@ -9,7 +9,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Microsoft.EntityFrameworkCore;
+using CadastroDeVendas.Data;
+using CadastroDeVendas.Models.ViewModels;
 namespace CadastroDeVendas
 {
     public class Startup
@@ -33,6 +35,9 @@ namespace CadastroDeVendas
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<CadastroDeVendasContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("CadastroDeVendasContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
