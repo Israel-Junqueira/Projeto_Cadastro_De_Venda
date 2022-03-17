ac359ae744390ca2032b183358d6c3847eb647af
Israel Ribeiro <israel.ribeiro313@gmail.com>
2022-03-17 12:55:01 -0300

    Adicionado a pagina de Erro

--- CadastroDeVendas/Controllers/SellersController.cs
+++ CadastroDeVendas/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using CadastroDeVendas.Services;
 using CadastroDeVendas.Models.ViewModels;
 using CadastroDeVendas.Models;
 using CadastroDeVendas.Services.Exceptions;
+using System.Diagnostics;
 
 namespace CadastroDeVendas.Controllers
 {
@@ -45,13 +46,13 @@ namespace CadastroDeVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
 
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
         }
@@ -67,13 +68,13 @@ namespace CadastroDeVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error),new {message = "Id not provided"});
             }
             var obj = _sellerService.FindById(id.Value);
 
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             return View(obj);
 
@@ -83,12 +84,12 @@ namespace CadastroDeVendas.Controllers
         {
             if (id == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not provided" });
             }
             var obj = _sellerService.FindById(id.Value);
             if (obj == null)
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = "Id not found" });
             }
             List<Department> departments = _departmentService.FindAll();
             SellerFormViewModel viewModel = new SellerFormViewModel { Seller = obj,Departments = departments };
@@ -100,20 +101,30 @@ namespace CadastroDeVendas.Controllers
         {
            if (id != seller.Id)
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = "Id not id mismatch" });
             }
             try
             {
                 _sellerService.Update(seller);
                 return RedirectToAction(nameof(Index));
-            }catch (NotFoundException)
+            }catch (NotFoundException e )
             {
-                return NotFound();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
-            catch (DbConcurrencyException)
+            catch (DbConcurrencyException e )
             {
-                return BadRequest();
+                return RedirectToAction(nameof(Error), new { message = e.Message });
             }
+        }
+
+        public IActionResult Error (string message)
+        {
+            var viewModel = new ErrorViewModel
+            {
+                Message = message,
+                RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier
+            };
+            return View(viewModel);
         }
     }
 }
